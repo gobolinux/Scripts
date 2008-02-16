153b3d9de1dfda73fe618af7b041021ccc2e07e1
Michael Homer <michael@gobolinux.org>
2008-02-16 08:23:38 +0000

    Example CommandNotFound data file. Contains only first ten entries generated from my system, plus two guaranteed fails "foo" and "quux".

---
+++ Data/CommandNotFound.py
@@ -0,0 +1,14 @@
+commands = {
+ '[': ['CoreUtils'],
+ '80211debug': ['Madwifi'],
+ '80211stats': ['Madwifi'],
+ 'a2p': ['Perl'],
+ 'a2ping': ['TeTeX'],
+ 'a2ps': ['A2PS'],
+ 'ab': ['HTTPD'],
+ 'acat': ['Atool'],
+ 'accept': ['CUPS'],
+ 'aclocal': ['Automake'],
+ 'foo': ['Foo', 'FooBar'],
+ 'quux': ['Quux'],
+}
