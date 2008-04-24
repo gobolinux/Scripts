51878c78eb1de52c1bd2384fd840f72ed5354a40
Lucas Correia Villa Real <lucasvr@gobolinux.org>
2008-04-24 12:29:43 +0000

    Moving contents to the body of libDependencies.c

--- src/libDependencies.h
+++ src/libDependencies.h
@@ -1,19 +1,5 @@
 #ifndef __libdependencies_h
 #define __libdependencies_h 1
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <libgen.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <dirent.h>
-
-#include "LinuxList.h"
-
-struct list_head *ParseDependencies(char *file, bool searchpackages, bool searchlocalprograms);
 
 #endif /* __libdependencies_h */
