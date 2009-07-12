fe938aaf125e75d1549caa75712be560f272d7d8
Jonas Karlsson <jonas@gobolinux.org>
2009-07-12 15:14:29 +0000

    Move $sudo_exec definition from Functions/GoboLinux.sh to bin/ScriptFunctions

--- Functions/GoboLinux.sh
+++ Functions/GoboLinux.sh
@@ -1,16 +1,5 @@
 #!/bin/sh (source)
 
-# Some simple define for sudo
-sudo_exec="sudo -u #0"
-sudo_validate="sudo -u `whoami` true"
-
-# If we can't sudo, we shouldn't try .
-if [ -n "$ROOTLESS_GOBOLINUX" ]
-then
-   sudo_exec=""
-   sudo_validate=""
-fi
-
 # Respawn the script, asking for root privileges
 Verify_Superuser_() {
    if [ -n "$ROOTLESS_GOBOLINUX" ]
